fecb62d8162476d9aa2096241d48510fba63d2cd
home <feitianniao2016@outlook.com>
2021-02-09 20:33:59 +0800

    00001

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;int l=0;bool a = true;
+            //1-100整数和
+            //do
+            //{
+            //    l += i; i++;
+            //} while (i != 101);
+            //Console.WriteLine(l);
+            //不断要求用户输入一个数字，输入end退出
+            //while (a == true)
+            //{
+            //    Console.WriteLine("输入一个数字，输入end退出");
+            // string s =Console.ReadLine();
+            //    if (s == "end") a = false;
+            //    else
+            //    {
+            //        try
+            //        {
+            //            i = Convert.ToInt32(s);
+            //            l += i;
+            //        }
+            //        catch
+            //        {
+            //            Console.WriteLine("输入格式不对");
+            //        }
+            //    }
+            //}
+            //Console.WriteLine("这些数字的和为{0}", l);
+        }
+    }
+}
