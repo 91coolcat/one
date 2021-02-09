6526b444d847ad7cbcf399ffea8f1547dbe21ab4
home <feitianniao2016@outlook.com>
2021-02-09 17:22:50 +0800

    0

--- 00001/Program.cs
+++ 00001/Program.cs
@@ -6,7 +6,20 @@ namespace _00001
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int v;
+            Console.WriteLine("时间   秒");
+            while (true)
+            {
+                try
+                {
+                    v = Convert.ToInt32(Console.ReadLine()); break;
+                }
+                catch
+                {
+                    Console.WriteLine("输入不规范");
+                }
+            }
+            Console.WriteLine("一共{0}分{1}秒", v / 60, v % 60);
         }
     }
 }
